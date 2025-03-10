83df62b0cd5497d7a29c0fc896070d2cf66b8edb
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-10 15:10:10 -0400

    Added main method and runExpt method

--- src/AgentSimulation.java
+++ src/AgentSimulation.java
@@ -7,7 +7,16 @@
 public class AgentSimulation {
 
     public static void main(String[] args){
-        
+        if (args.length != 1){
+            System.out.println("Usage: java AgentSimulation <N>");
+        }
+        else{
+            int N = Integer.parseInt(args[0]);
+            runExpt(N);
+        }
+    }
+
+    public static int runExpt(int N){
     }
     
 }
