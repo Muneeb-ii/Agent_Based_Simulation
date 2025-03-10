07d7d048dd8161438c0c9e913764f94013a80dee
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-10 15:08:03 -0400

    Create AgentSimulation.java

---
+++ src/AgentSimulation.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Muneeb Azfar Nafees
+ * 
+ * Purpose of the class: Implementation of an Agent Simulation
+ */
+
+public class AgentSimulation {
+
+    public static void main(String[] args){
+        
+    }
+    
+}
