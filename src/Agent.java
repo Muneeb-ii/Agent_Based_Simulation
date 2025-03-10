394466e41b5e56c7cc7f3557c64756a5046d0172
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-10 04:43:06 -0400

    Added a constructor

--- src/Agent.java
+++ src/Agent.java
@@ -5,5 +5,21 @@
  */
 
  abstract class Agent{
-    
+    private double x;
+    private double y;
+    protected int radius;
+    protected boolean moved;
+
+    /**
+     * Constructor for the Agent class
+     * @param x0 the x-coordinate of the agent
+     * @param y0 the y-coordinate of the agent
+     * @param radius the radius of the agent
+     */
+    public Agent(double x0, double y0, int radius){
+        x = x0;
+        y = y0;
+        this.radius = radius;
+        moved = false;
+    }
  }
