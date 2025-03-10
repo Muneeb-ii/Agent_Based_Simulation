4dc6756d28bffe2182f3a6a85a0ccb009a2ccdb7
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-10 08:07:49 -0400

    Downloaded LandscapeTests class

---
+++ src/LandscapeTests.java
@@ -0,0 +1,43 @@
+/*
+file name:      LandscapeTests.java
+Authors:        Ike Lage
+last modified:  03/04/2025
+
+How to run:     java -ea LandscapeTests
+*/
+
+public class LandscapeTests {
+
+    public static double landscapeTests() {
+
+        double score = 0.;
+
+        // case 1: testing Landscape(int, int)
+        {
+
+            //TODO
+        
+        }
+
+        // case 2: testing getWidth()
+        {
+
+            //TODO
+
+        }
+
+        // case 3: testing getHeight()
+        {
+            
+            //TODO
+
+        }
+
+        return score ;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println( landscapeTests() );
+    }
+}
