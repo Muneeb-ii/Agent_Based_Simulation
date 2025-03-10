659c9c8397bd4f4aaa8c7e827a62b3a86e345253
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-10 07:27:20 -0400

    Downloaded AgentTests class

---
+++ src/AgentTests.java
@@ -0,0 +1,65 @@
+/*
+file name:      AgentTests.java
+Authors:        Ike Lage
+last modified:  03/04/2025
+
+How to run:     java -ea AgentTests
+*/
+
+public class AgentTests {
+
+    public static double agentTests() {
+
+        double score = 0. ;
+
+        // case 1: testing SocialAgent( double , double , int ) and AntiSocialAgent( double , double , int )
+        {
+            //TODO
+        }
+
+        // case 2: testing getX() for both SocialAgent and AntiSocialAgent
+        {
+            //TODO
+        }
+
+        // case 3: testing getY() for both SocialAgent and AntiSocialAgent
+        {
+            //TODO
+            
+        }
+
+        // case 4: testing getRadius() for both SocialAgent and AntiSocialAgent
+        {
+            //TODO
+        }
+
+        // case 5: testing setX() for both SocialAgent and AntiSocialAgent
+        {
+            //TODO
+        }
+
+        // case 6: testing setY()  for both SocialAgent and AntiSocialAgent
+        {
+            //TODO
+        }
+
+        // case 7: testing setRadius()  for both SocialAgent and AntiSocialAgent
+        {
+            //TODO
+        }
+
+        // case 8: testing getMoved()  for both SocialAgent and AntiSocialAgent
+        {
+            //TODO
+        }
+
+        return score ;
+    }
+    
+
+
+    public static void main(String[] args) {
+
+        System.out.println( agentTests() );
+    }
+}
